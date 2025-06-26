687bb797b90898a67e212f11a16c7ffaae7d1e21
ryo <oresama5656@yahoo.co.jp>
2025-06-26 17:03:29 +0900

    Add Digital Asset Links for TWA support

---
+++ app/src/main/java/com/coommu/quiz/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.coommu.quiz.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
